3e1d82943c10e6ee6b1c18d6fba52aabda57b01c
Dustin Sinkey <dustin@cherre.com>
2022-09-28 15:46:32 -0300

    Refactor Exchange component to use helper function and use ContentGrid component

--- src/components/Exchanges/Exchange.jsx
+++ src/components/Exchanges/Exchange.jsx
@@ -3,11 +3,10 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Grid from "@mui/material/Grid";
+import ContentGrid from "../ContentGrid";
 import { useNavigate } from "react-router-dom";
+import { btcFormat } from "../../helpers";
 
 const ExchangeCard = ({ exchange }) => {
   const navigate = useNavigate();
@@ -19,10 +18,39 @@ const ExchangeCard = ({ exchange }) => {
     [navigate]
   );
 
-  const btcFormat = Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "BTC",
-  });
+  const formateBTC = btcFormat();
+
+  const gridContents = [
+    {
+      content: `Trust Score Rank: ${exchange?.trust_score_rank}`,
+      size: 6,
+    },
+    {
+      content: `Trust Score: ${exchange?.trust_score}`,
+      size: 6,
+    },
+    {
+      content: `Trading Volume 24 Hour: ${formateBTC.format(
+        exchange?.trade_volume_24h_btc
+      )}`,
+      size: 6,
+    },
+    {
+      content: `Trading Volume 24 Hour Normalized: ${formateBTC.format(
+        exchange?.trade_volume_24h_btc
+      )}`,
+      size: 6,
+    },
+    {
+      content: `Country: ${exchange.country ? exchange.country : "N/A"}`,
+      size: 6,
+    },
+    {
+      content: `Centralized: ${exchange?.centralized?.toString()}`,
+      size: 6,
+    },
+  ];
+
   return (
     <Card
       sx={{
@@ -38,32 +66,7 @@ const ExchangeCard = ({ exchange }) => {
         subheader={`Established in ${exchange.year_established}`}
       />
       <CardContent>
-        <Grid container spacing={2} sx={{ marginBottom: 2 }}>
-          <Grid item xs={6}>
-            {`Trust Score Rank: ${exchange.trust_score_rank}`}
-          </Grid>
-          <Grid item xs={6}>
-            {`Trust Score: ${exchange.trust_score}`}
-          </Grid>
-          <Grid item xs={6}>
-            {`Trading Volume 24 Hour: ${btcFormat.format(
-              exchange.trade_volume_24h_btc
-            )}`}
-          </Grid>
-          <Grid item xs={6}>
-            {`Trading Volume 24 Hour Normalized: ${btcFormat.format(
-              exchange.trade_volume_24h_btc
-            )}`}
-          </Grid>
-          <Grid item xs={6}>
-            {`Country: ${exchange.country ? exchange.country : "N/A"}`}
-          </Grid>
-          <Grid item xs={6}>
-            {`Trading Volume 24 Hour Normalized: ${btcFormat.format(
-              exchange.trade_volume_24h_btc
-            )}`}
-          </Grid>
-        </Grid>
+        <ContentGrid gridContents={gridContents} />
         <Typography variant="body2" color="text.secondary">
           {exchange.description}
         </Typography>
--- src/components/Exchanges/Exchanges.jsx
+++ src/components/Exchanges/Exchanges.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import ExchangeCard from "./Exchange";
 
 const Exchanges = ({ exchanges }) => {
