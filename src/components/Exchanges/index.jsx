fd18dc6e19bd13362b24e82698fa362307c35f33
Dustin Sinkey <dustin@cherre.com>
2022-10-18 06:16:36 -0300

    Move buttons up to the top so you don't have to keep searching for them, added handlers for onClick, andpage number

--- src/components/Exchanges/index.jsx
+++ src/components/Exchanges/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Stack } from "@mui/material";
+import { Stack, Button } from "@mui/material";
 import ExchangeCard from "./ExchangeCard";
 
 const Exchanges = ({ exchanges }) => {
   const [displayExchanges, setDisplayExchanges] = useState(exchanges);
   const [page, setPage] = useState(0);
 
+  const onPageDownClick = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const onPageUpClick = () => {
+    if (page < exchanges.length / 10 - 1) {
+      setPage(page + 1);
+    }
+  };
+
   useEffect(() => {
-    if (page >= 0) {
+    if (page >= 0 && page < exchanges.length / 10) {
       const returnData = exchanges.slice(page * 10, page * 10 + 10);
 
       setDisplayExchanges(returnData);
@@ -16,15 +28,16 @@ const Exchanges = ({ exchanges }) => {
 
   return (
     <div>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <Button onClick={onPageDownClick}>down</Button>
+        <Button onClick={onPageUpClick}>up</Button>
+        <div>{`Page ${page + 1}`}</div>
+      </div>
       <Stack spacing={3} justifyContent="center">
         {displayExchanges.map((exchange, index) => (
           <ExchangeCard key={index} exchange={exchange} index={index} />
         ))}
       </Stack>
-      <div>
-        <button onClick={() => setPage(page - 1)}>down</button>
-        <button onClick={() => setPage(page + 1)}>up</button>
-      </div>
     </div>
   );
 };
